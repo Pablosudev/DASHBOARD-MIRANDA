2a918e496e52ef5442cc9fd975d09cfbfc952ab7
Pablosudev <pablo.losu.dev@gmail.com>
2025-01-15 11:30:04 +0100

    first commit

---
+++ src/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+import { DashboardMenu } from "./components/DashboardMenu"
+
+
+
+
+export const Dashboard = () => {
+
+
+    return ( 
+        <>
+        <Outlet/>
+        </>
+    )
+}
