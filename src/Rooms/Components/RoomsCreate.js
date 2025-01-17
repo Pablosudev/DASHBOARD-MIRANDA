867c176f417139e3cb3209b5b86b89558f110f32
Pablosudev <pablo.losu.dev@gmail.com>
2025-01-17 19:19:41 +0100

    room create

--- src/Rooms/Components/RoomsCreate.js
+++ src/Rooms/Components/RoomsCreate.js
@@ -1,22 +1,67 @@
 import styled from "styled-components";
-import Swiper from "swiper";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 
 
 export const CardCreate = styled.section`
 margin-top: 10%;
-margin-left:15%;
+margin-left:10%;
 background-color: #FFFFFF;
 border-radius: 12px;
-width:10rem;
+width:92rem;
 max-height:49.5rem;
 display: flex;
+justify-content:space-between;
+position:relative;
 `
-export const SwiperRooms = styled(Swiper)`
-width: 10px;
-height: 30px;
+export const ContainerSections = styled.div`
+width:92rem;
 `
-export const ImgSwiper = styled.img`
+export const ImgSlide = styled.img`
 width:46rem;
 height:49.5rem;
+border-bottom-right-radius: 12px;
+border-top-right-radius: 12px;
+`
+export const IconArrowRight = styled(FaLongArrowAltRight)`
+font-size: 1rem;
+color: #ffffff;
+`
+export const IconArrowLeft = styled(FaLongArrowAltLeft)`
+font-size: 1rem;
+color: #ffffff;
+`
+export const ButtonSlideRight = styled.button`
+background-color:#C5C5C5;
+position:absolute;
+bottom:25%;
+right: 0%;
+`
+export const ButtonSlideLeft = styled.button`
+background-color:#C5C5C5;
+position:absolute;
+bottom:25%;
+right:45%;
+`
+export const TitleSection = styled.h3`
+font-family: "Poppins";
+`
+export const BoxTitle = styled.div`
+margin-top: 3rem;
+align-items: center;
+border-bottom:2px solid #F8F8F8;
+display: flex;
+justify-content: space-around;
+margin-left: 3rem;
+margin-right:3rem;
+`
+export const RoomInfo = styled.h4`
+color: #6E6E6E;
+font-family: "Poppins";
+margin-left: 4rem;
+margin-right: 5rem;
+`
+export const BoxInfo = styled.div`
+display:flex;
 `
--- src/Rooms/Pages/RoomsCreate.jsx
+++ src/Rooms/Pages/RoomsCreate.jsx
@@ -1,15 +1,71 @@
-
+import { useState } from 'react';
+import {BoxInfo , RoomInfo, ButtonSlideLeft ,ButtonSlideRight ,BoxTitle ,IconArrowLeft, IconArrowRight, ImgSlide, TitleSection, ContainerSections } from '../Components/RoomsCreate';
 import { CardCreate } from '../Components/RoomsCreate';
 
-export const RoomsCreate = () => {
-  return (
 
+export const RoomsCreate = () => {
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const images = [
+    "/src/assets/Imagenes/room1.jpg",
+    "/src/assets/Imagenes/room2.jpg",
+    "/src/assets/Imagenes/room5.jpg",
+    "/src/assets/Imagenes/room4.jpg"
+  ];
+
+  const handleButtonNext = () => {
+    setCurrentImage ((prevIndex) => (prevIndex +1) % images.length);
+  };
+  const handleButtonPrev = () => {
+    setCurrentImage ((prevIndex) => (prevIndex -1 + images.length ) % images.length)
+  };
+
+  return (
+  
     <CardCreate>
+        <ContainerSections>
+            <BoxTitle>
+              <div>
+                <TitleSection>Room Type</TitleSection>
+                <input type="text" />
+              </div>
+              <div>
+                <TitleSection>Room Number</TitleSection>
+                <input type="text" />
+              </div>
+            </BoxTitle>
+            <BoxInfo>
+              <div>
+                <RoomInfo>Room Info</RoomInfo>
+                <p>p</p>
+              </div>
+              <div>
+                <RoomInfo>Price</RoomInfo>
+                <p>p</p>
+              </div>
+              <div>
+                <RoomInfo>Offer</RoomInfo>
+                <p>p</p>
+              </div>
+            </BoxInfo>
+            
+            <TitleSection>Description</TitleSection>
+            <p>jsgijfs</p>
+            <TitleSection>Offer</TitleSection>
+            <TitleSection>Price</TitleSection>
+            <p>dffddffd</p>
+            <TitleSection>Discount</TitleSection>
+            <p>fjdhnf</p>
+            <TitleSection>Cancellation</TitleSection>
+            <p>Campor de texto</p>
+            <TitleSection>Amenities</TitleSection>
+            <p>fdaidbk</p>
+        </ContainerSections>
         <div>
-            <h1>hola</h1>
-        </div>
-        <div>
-        
+          <ImgSlide src= {images[currentImage]} alt="photo room 1" />
+          <ButtonSlideRight onClick={handleButtonNext}><IconArrowRight/></ButtonSlideRight>
+          <ButtonSlideLeft onClick={handleButtonPrev}><IconArrowLeft/></ButtonSlideLeft>
         </div>
     </CardCreate>
     
