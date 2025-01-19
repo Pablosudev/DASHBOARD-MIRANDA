909e13bb3e13be801ebb1644d468486cceee4a65
Pablosudev <pablo.losu.dev@gmail.com>
2025-01-19 18:56:26 +0100

    Bookings

--- src/Bookings/Components/BookingsDetails.js
+++ src/Bookings/Components/BookingsDetails.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { FaLongArrowAltLeft } from "react-icons/fa";
 import { FaLongArrowAltRight } from "react-icons/fa";
 import { FaPhoneAlt } from "react-icons/fa";
-
+import { LuMessageCircleMore } from "react-icons/lu";
 
 export const CardBookings = styled.section`
 background-color: #ffffff;
@@ -10,13 +10,19 @@ margin-left: 4%;
 margin-top:10%;
 border-radius:0.8rem; 
 display: flex; 
+max-width: 92rem;
+max-height: 47rem;
+`
+export const CardInfo = styled.article`
+padding-left: 4rem;
+padding-right: 4rem;
 `
 export const CardSlide = styled.article`
 position: relative;
 `
 export const ImgSlide = styled.img`
 width:46rem;
-height:49.5rem;
+height:47rem;
 border-bottom-right-radius: 12px;
 border-top-right-radius: 12px;
 `
@@ -61,4 +67,126 @@ padding-right:1rem;
 `
 export const PhoneIcon = styled(FaPhoneAlt )`
 color:#135846;
+border: 1px solid #E8F2EF;
+padding: 1rem;
+border-radius: 0.8rem;
+margin-right:2rem;
+`
+export const IconMessage = styled(LuMessageCircleMore)`
+color: #ffffff;
+font-size: 1.3rem;
+margin-right: 0.5rem;
+`
+export const ButtonMessage = styled.button`
+background-color: #135846;
+border: transparent;
+color: #ffffff;
+font-family: "Poppins";
+font-size:1rem;
+padding-left: 2rem;
+padding-right: 2rem;
+padding-bottom: 0%.9;
+padding-top: 0%.9 ;
+border-radius: .8rem; 
+display: flex;
+align-items: center;
+`
+export const IdBookings = styled.p`
+font-family: "Poppins";
+font-size: 0.8rem;
+color:#799283;
+text-align: center;
+`
+export const BoxMessage = styled.div`
+display: flex;
+padding-left: 8rem;
+margin-top: 0.5rem;
+margin-bottom: 0.5rem;
+`
+export const ContainerDetails = styled.article`
+display: flex;
+border-bottom:2px solid #C5C5C5;
+margin-right: 1rem;
+margin-left: 1rem;
+`
+export const BoxCheck = styled.div`
+margin-right: 2rem;
+margin-left: 2rem;
+text-align:left;
+
+`
+export const TitleData = styled.h3`
+font-family: "Poppins";
+font-size: 0.87rem;
+color: #6E6E6E;
+`
+export const DataCheck = styled.p`
+font-family: "Poppins";
+font-size: 1rem; 
+`
+export const NameBooking = styled.h1`
+font-family: "Poppins";
+font-size: 1.8rem;
+text-align: center;
+margin-bottom: 0%;
+`
+export const Night = styled.p`
+color: #799283;
+font-family: "Poppins";
+font-size: 0.87rem;
+margin-top: 0%;
+`
+export const BoxPrice = styled.div`
+display: flex;
+align-items: center;
+`
+export const Price = styled.p`
+font-family: "Poppins";
+font-size: 1.5rem;
+margin-right: 0.5rem;
+margin-top: 0%;
+`
+export const TypeRoom = styled.p`
+font-family: "Poppins";
+font-size: 1.5rem;
+margin-top: 1rem;
+`
+export const ContainerRoom = styled.div`
+display: flex;
+margin-top: 2rem;
+`
+export const Request = styled.p`
+font-family: "Poppins";
+font-size: 0.87rem;
+text-align: left;
+`
+export const BoxRoom = styled.div`
+margin-left: 3rem;
+margin-right: 2rem;
+`
+export const ButtonDetails = styled.button`
+border-radius: 0.8rem;
+border: 1px solid #135846;
+background-color:#135846;
+color: #ffffff;
+padding-top: 0.5rem;
+padding-bottom:0.5rem;
+margin-right: 0.5rem;
+margin-bottom: 0.5rem;
+`
+export const TypeSlide = styled.h3`
+font-family: "Poppins";
+font-size: 1.5rem;
+color: #ffffff;
+`
+export const DescriptionRoom = styled.p`
+font-family: "Poppins";
+font-size: 0.8rem;
+color: #ffffff;
+`
+export const BoxDescription = styled.div`
+position: absolute;
+bottom:  5%; 
+padding-left: 2rem;
+padding-right: 2rem;
 `
--- src/Bookings/Pages/BookingsDetails.jsx
+++ src/Bookings/Pages/BookingsDetails.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ImgSlide, ButtonSlideLeft, ButtonSlideRight, IconArrowLeft, IconArrowRight, CardBookings, CardSlide, PhoneIcon } from "../Components/BookingsDetails";
+import {TypeSlide , BoxDescription , ButtonDetails , BoxRoom , CardInfo ,ContainerRoom ,TypeRoom, BoxPrice, Price, ImgSlide,Night, ButtonSlideLeft, ButtonSlideRight, IconArrowLeft, IconArrowRight,DescriptionRoom, CardBookings, CardSlide, PhoneIcon, IconMessage, ButtonMessage, BoxMessage, ContainerDetails, BoxCheck, TitleData, DataCheck, NameBooking, IdBookings , Request } from "../Components/BookingsDetails";
 export const BookingsDetails = () => {
 
 
@@ -22,19 +22,61 @@ const [currentImage, setCurrentImage] = useState(0);
 return (
     <>
     <CardBookings>
-      <article>
-        <h1>Roberto Mansini</h1>
-        <p>ID 1234124512551</p>
-        <PhoneIcon/>
-        <button></button>
-      </article>
+      <CardInfo>
+        <NameBooking>Roberto Mansini</NameBooking>
+        <IdBookings>ID 1234124512551</IdBookings>
+        <BoxMessage>
+          <PhoneIcon/>
+          <ButtonMessage> <IconMessage/> Send Message</ButtonMessage>
+        </BoxMessage>
+        <ContainerDetails>
+          <BoxCheck>
+            <TitleData>Check In</TitleData>
+            <DataCheck>October 30th, 2025 | 8:23 AM </DataCheck>
+          </BoxCheck>
+          <BoxCheck>
+            <TitleData>Check Out</TitleData>
+            <DataCheck>November 2th, 2025</DataCheck>
+          </BoxCheck>
+        </ContainerDetails>
+        <ContainerRoom>
+          <BoxRoom>
+            <TitleData>Room Info</TitleData>
+            <TypeRoom>Deluxe Z - 002424</TypeRoom>
+          </BoxRoom>
+          <BoxRoom>
+            <TitleData>Price</TitleData>
+            <BoxPrice>
+              <Price>$145</Price>
+              <Night>/night</Night>
+            </BoxPrice>
+          </BoxRoom>
+        </ContainerRoom>
+        <Request>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</Request>
+        <TitleData>Amenities</TitleData>
+        <div>
+          <ButtonDetails>FREE WIFI</ButtonDetails>
+          <ButtonDetails>TV LED</ButtonDetails>
+          <ButtonDetails>2 BATHROOM</ButtonDetails>
+          <ButtonDetails>AC</ButtonDetails>
+          <ButtonDetails>3 BED SPACE</ButtonDetails>
+          <ButtonDetails>COFEE SET</ButtonDetails>
+          <ButtonDetails>BATHUP</ButtonDetails>
+          <ButtonDetails>TOWEL</ButtonDetails>
+          <ButtonDetails>SHOWER</ButtonDetails>
+        </div>
+      </CardInfo>
       <CardSlide>
         <ImgSlide src= {images[currentImage]} alt="photo room 1" />
         <ButtonSlideRight onClick={handleButtonNext}><IconArrowRight/></ButtonSlideRight>
         <ButtonSlideLeft onClick={handleButtonPrev}><IconArrowLeft/></ButtonSlideLeft>
+        <BoxDescription>
+          <TypeSlide>Deluxe</TypeSlide>
+          <DescriptionRoom>Lorem ipsum dolor sit amet, consectetur adispiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam , quis nostrud exerci.</DescriptionRoom>
+        </BoxDescription>
       </CardSlide>
+
     </CardBookings>
-    
     </>
 )
 
